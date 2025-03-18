2a0658696dabb0d0390816b8d360d7c040abe6e5
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2025-03-17 21:09:43 -0700

    Rutas agregadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './pages/principal/principal.component';
+import { Gta3Component } from './pages/gta3/gta3.component';
+import { ViceCityComponent } from './pages/vice-city/vice-city.component';
+import { SanAndreasComponent } from './pages/san-andreas/san-andreas.component';
+import { RedDeadRedemptionComponent } from './pages/red-dead-redemption/red-dead-redemption.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'principal', pathMatch: 'full'},
-{path: 'principal', component: PrincipalComponent},
+  {path: 'principal', component: PrincipalComponent},
+  {path: 'gta3', component: Gta3Component},
+  {path: 'vicecity', component: ViceCityComponent},
+  {path: 'sanandreas', component: SanAndreasComponent},
+  {path: 'reddeadredemption', component: RedDeadRedemptionComponent}
 ];
 
 @NgModule({
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: false,
+  providers: [RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
